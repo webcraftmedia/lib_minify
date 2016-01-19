6160d32c2a231adcb65bba18e6cf8dc464c2534d
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-01-19 21:42:20 +0100

    path fixes

--- lib_minify.php
+++ lib_minify.php
@@ -4,7 +4,10 @@ class lib_minify extends \LIB\lib_php{
     public static function get_class(){
         return self::class;}
     public static function php_autoload(){
-        \SYSTEM\autoload::registerFolder(\dirname(__FILE__).'lib/src/','MatthiasMullie\Minify');
+        require_once dirname(__FILE__).'/lib/src/Minify.php';
+        require_once dirname(__FILE__).'/lib/src/CSS.php';
+        require_once dirname(__FILE__).'/lib/src/Exception.php';
+        require_once dirname(__FILE__).'/lib/src/JS.php';
         return true;}
     public static function version(){
         return 'https://github.com/matthiasmullie/minify';}
