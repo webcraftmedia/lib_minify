24cd061fd89ec526909afafefffff9af50725c04
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-10-27 21:52:37 +0200

    add missing includes

--- lib_minify.php
+++ lib_minify.php
@@ -4,6 +4,8 @@ class lib_minify extends \LIB\lib_php{
     public static function get_class(){
         return self::class;}
     public static function php_autoload(){
+        require_once dirname(__FILE__).'/dep/src/ConverterInterface.php';
+        require_once dirname(__FILE__).'/dep/src/Converter.php';
         require_once dirname(__FILE__).'/lib/src/Minify.php';
         require_once dirname(__FILE__).'/lib/src/CSS.php';
         require_once dirname(__FILE__).'/lib/src/Exception.php';
